983e312263c908235fe15e2f8f999d7cd9c3a88b
aryaajithkumar <aryaajithkumar96@gmail.com>
2025-07-21 11:38:03 +0530

    1

---
+++ src/programs/Removeduplicates.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i,k;
+        for(i=0;i<nums.length;i++){
+            int[i]==int[i+1];
+            int[i]=int[i+1]
+
+        }
+        return int
+    }
+    public static void main(String[] args) {
+        int[] nums = [1,1,2,3,4,4];
+        removeDuplicates(nums);
+
+    }
+}
